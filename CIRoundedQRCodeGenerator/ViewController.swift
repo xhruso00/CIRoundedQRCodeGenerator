88a01583307c18ae29e309b49115c4c420bff787
Marek Hrusovsky <xhruso00@gmail.com>
2025-10-07 16:55:00 +0700

    Initial Commit

---
+++ CIRoundedQRCodeGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CIRoundedQRCodeGenerator
+//
+//  Created by Marek Hrusovsky on 07/10/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
